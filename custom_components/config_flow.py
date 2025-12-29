e8a8d168072ff4e64aca99f372e7c91d9ec84d31
Nicola Scarabello <nicola.scarabello@virtual-logic.it>
2025-12-29 15:02:38 +0100

    test config flow

--- custom_components/config_flow.py
+++ custom_components/config_flow.py
@@ -1,5 +1,24 @@
 
 from homeassistant import config_entries
 
+import voluptuous as vol
+
+DATA_SCHEMA = vol.Schema({("host"): str})
+
 class ExampleConfigFlow(config_entries.ConfigFlow, domain="test_integration"):
-    pass
+     VERSION = 1
+
+     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        # This goes through the steps to take the user through the setup process.
+        # Using this it is possible to update the UI and prompt for additional
+        # information. This example provides a single form (built from `DATA_SCHEMA`),
+        # and when that has some validated input, it calls `async_create_entry` to
+        # actually create the HA config entry. Note the "title" value is returned by
+        # `validate_input` above.
+        errors = {}
+
+        # If there is no user input or there were errors, show the form again, including any errors that were found with the input.
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
